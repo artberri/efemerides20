93f4852bb7251c61e0fe2d0549e720a0b9307a7e
Alberto Varela <alberto@berriart.com>
2022-11-03 19:30:09 +0100

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,51 @@
+const isStaticPage = (path) => path.startsWith("/pagina/")
+const isHome = (path) => path === "/"
+const isTopics = (path) => path === "/nodos"
+const isTopic = (path) => path.startsWith("/nodos/")
+const isYearList = (path) => path === "/anos"
+const isYear = (path) => path.startsWith("/anos/")
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: process.env.SITE_URL || "https://www.efemerides20.com",
+	generateRobotsTxt: false,
+	generateIndexSitemap: false,
+	outDir: "out",
+	changefreq: "monthly",
+	transform: async (config, path) => {
+		if (isStaticPage(path)) {
+			return null
+		}
+
+		const output = {
+			loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+			changefreq: config.changefreq,
+			priority: config.priority,
+			lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+			alternateRefs: config.alternateRefs ?? [],
+		}
+
+		if (isHome(path)) {
+			output.priority = 1
+		}
+
+		if (isTopics(path)) {
+			output.priority = 0.4
+		}
+
+		if (isTopic(path)) {
+			output.priority = 0.8
+		}
+
+		if (isYearList(path)) {
+			output.priority = 0.4
+			output.changefreq = "yearly"
+		}
+
+		if (isYear(path)) {
+			output.priority = 0.5
+		}
+
+		return output
+	},
+}
